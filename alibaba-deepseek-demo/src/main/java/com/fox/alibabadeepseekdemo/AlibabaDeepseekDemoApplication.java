3a99bb81065659e86334c756aee742b99c9e4ec8
sicongdaxia <3495580899@qq.com>
2025-08-02 21:26:12 +0800

    1111

--- alibaba-deepseek-demo/src/main/java/com/fox/alibabadeepseekdemo/AlibabaDeepseekDemoApplication.java
+++ alibaba-deepseek-demo/src/main/java/com/fox/alibabadeepseekdemo/AlibabaDeepseekDemoApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //你好a
 @SpringBootApplication
 public class AlibabaDeepseekDemoApplication {
-//    傻逼赵一涵6666
     public static void main(String[] args) {
         SpringApplication.run(AlibabaDeepseekDemoApplication.class, args);
     }
