856334395f780460177322ca81e002f4799771ff
X1-YE <3284680748@qq.com>
2025-08-02 22:00:11 +0800

    测试

--- alibaba-deepseek-demo/src/main/java/com/fox/alibabadeepseekdemo/AlibabaDeepseekDemoApplication.java
+++ alibaba-deepseek-demo/src/main/java/com/fox/alibabadeepseekdemo/AlibabaDeepseekDemoApplication.java
@@ -3,7 +3,7 @@ package com.fox.alibabadeepseekdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+//注释测试可删
 @SpringBootApplication
 public class AlibabaDeepseekDemoApplication {
     public static void main(String[] args) {
