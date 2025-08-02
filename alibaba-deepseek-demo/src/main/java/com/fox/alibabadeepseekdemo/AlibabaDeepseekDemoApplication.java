6926031f88be4f971947cb1011aa1be77df5c846
_dust <163943585+lisonowo@users.noreply.github.com>
2025-08-02 20:45:04 +0800

    已添加注释
    Co-Authored-By: liyi <139129954+lee666nb@users.noreply.github.com>

--- alibaba-deepseek-demo/src/main/java/com/fox/alibabadeepseekdemo/AlibabaDeepseekDemoApplication.java
+++ alibaba-deepseek-demo/src/main/java/com/fox/alibabadeepseekdemo/AlibabaDeepseekDemoApplication.java
@@ -3,6 +3,7 @@ package com.fox.alibabadeepseekdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//你好
 @SpringBootApplication
 public class AlibabaDeepseekDemoApplication {
 
