bcacf3ae99e66676683c87219587119efc35455f
_dust <163943585+lisonowo@users.noreply.github.com>
2025-08-02 21:00:33 +0800

    Update AlibabaDeepseekDemoApplication.java
    123
    
    Co-Authored-By: liyi <139129954+lee666nb@users.noreply.github.com>

--- alibaba-deepseek-demo/src/main/java/com/fox/alibabadeepseekdemo/AlibabaDeepseekDemoApplication.java
+++ alibaba-deepseek-demo/src/main/java/com/fox/alibabadeepseekdemo/AlibabaDeepseekDemoApplication.java
@@ -3,7 +3,7 @@ package com.fox.alibabadeepseekdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//你好
+//你好a
 @SpringBootApplication
 public class AlibabaDeepseekDemoApplication {
 
